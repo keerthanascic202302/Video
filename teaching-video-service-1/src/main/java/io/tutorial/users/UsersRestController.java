dd5595f74b409386601d9c69fef7981fc04bd452
keerthanascic202302 <keerthanascic202302@gmail.com>
2023-05-17 11:31:44 +0530

    added file upload

--- teaching-video-service-1/src/main/java/io/tutorial/users/UsersRestController.java
+++ teaching-video-service-1/src/main/java/io/tutorial/users/UsersRestController.java
@@ -1,10 +1,14 @@
 package io.tutorial.users;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,8 +17,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 
 @RestController
@@ -28,6 +34,29 @@ public class UsersRestController {
 	public String firstPage() {
 		return "Hello World";
 	}
+	
+	
+	@RequestMapping(value = "/upload",
+			method = RequestMethod.POST,
+			consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+
+	public String fileUpload(@RequestParam("file") MultipartFile file) throws IOException
+	{
+		File convertFile = new File("D:/work/" + file.getOriginalFilename());
+		convertFile.createNewFile();
+
+		try (FileOutputStream fout = new FileOutputStream(convertFile))
+		{
+			fout.write(file.getBytes());
+		}
+		catch (Exception exe)
+		{
+			exe.printStackTrace();
+		}
+		return "File has uploaded successfully";
+	}
+	
+	
 	
 	@PostMapping("/user")
 	public ResponseEntity<String> createUsers(@RequestBody Users users){
